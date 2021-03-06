49df8efb8b233f6655a763673d61387cb21f5ef2
NandoFilter <fernandohfilter10@gmail.com>
2021-03-06 15:03:29 -0300

    Added Onibus program

--- src/br/univates/poo_2021a/pratica04/Onibus.java
+++ src/br/univates/poo_2021a/pratica04/Onibus.java
@@ -15,9 +15,25 @@ public class Onibus {
     /** Um ônibus. O ônibus tem um limite de passageiros, eles podem
      * entrar ou sair do ônibus e, claro, pagam um valor de passagem. */ 
 
-    int pas, totalPas, paradas;
+    int pas = 0;
+    int totalPas = 0;
+    int paradas = 0;
     int capacidade = 50;
-    double total;
+    double valor = 5.5;
+    double total = 0;
 
+    public void addPassageiros(int passageiros){
+        this.pas += passageiros;
+
+        if(passageiros > 0){
+           this.totalPas += passageiros;
+        }
+
+        paradas++;
+    }
+
+    public double valorTotal(){
+       return this.total = this.getPas() * this.valor;
+    }
 
 }
--- src/br/univates/poo_2021a/pratica04/ProgramaOnibus.java
+++ src/br/univates/poo_2021a/pratica04/ProgramaOnibus.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /**
  * @author NandoFilter
- * @date 03/04/2021
+ * @date 03/06/2021
  */
 
 public class ProgramaOnibus {
@@ -14,14 +14,28 @@ public class ProgramaOnibus {
 
         Onibus onibus = new Onibus();
 
-        System.out.println("Informe a quantidade de pessoas na parada [0 para Cancelar]: ");
-        int pas = scan.nextInt();
-        onibus.setPas(pas);
+        while(true) {
+            System.out.print("Informe a movimentação de pessoas na parada [Cancelar -> 0]: ");
+            int pas = scan.nextInt();
+            onibus.addPassageiros(pas);
 
-        if(pas != 0){
-            System.out.println(onibus.getPas());
-        }else{
-
+            if (pas != 0) {
+                if (onibus.getPas() > onibus.getCapacidade()) {
+                    System.out.println("Limite máximo de " + onibus.getCapacidade() + " pessoas.");
+                    onibus.setPas(onibus.getCapacidade());
+                } else if (onibus.getPas() < 0) {
+                    System.out.println("[Error] Não há essa quantidade de passageiros dentro do Ônibus para sair");
+                    onibus.setPas(0);
+                }
+                System.out.println("Passageiros: " + onibus.getPas());
+                System.out.println("Total de Passageiros: " + onibus.getTotalPas());
+                System.out.println("Lugares Vagos: " + (onibus.getCapacidade() - onibus.getTotalPas()));
+            } else {
+                System.out.println("Valor Total: R$");
+                System.out.printf("%.2f", onibus.valorTotal());
+                System.out.println("Foram feitas " + onibus.getParadas() + " paradas com o total de " + onibus.getTotalPas() + " passageiros no dia.");
+                break;
+            }
         }
     }
 }
