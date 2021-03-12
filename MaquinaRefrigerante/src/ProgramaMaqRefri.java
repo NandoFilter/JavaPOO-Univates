25955bfb93bb8fb3eff8cdcf632328132b8af7db
NandoFilter <fernandohfilter10@gmail.com>
2021-03-12 00:20:51 -0300

    03/12/2021 Added MaquinaRefri program

---
+++ MaquinaRefrigerante/src/ProgramaMaqRefri.java
@@ -0,0 +1,16 @@
+/**
+ * @author NandoFilter
+ * @date 03/11/2021
+ */
+
+public class ProgramaMaqRefri {
+    public static void main(String[] args) {
+
+        MaquinaRefri copo1 = new MaquinaRefri();
+        
+        copo1.reabastecer();
+
+        copo1.status();
+
+    }
+}
