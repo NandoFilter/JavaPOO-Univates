6b7eac1aeaa8c0e74104b7094ccb6ac543032545
NandoFilter <fernandohfilter10@gmail.com>
2021-04-17 18:39:09 -0300

    04/17/21 - Create TelaRadioMain.java

---
+++ src/br/univates/poo_2021a/pratica13/prova2020B/TelaRadioMain.java
@@ -0,0 +1,14 @@
+package br.univates.poo_2021a.pratica13.prova2020B;
+
+/**
+ * @author NandoFilter
+ * @date 04/17/2021
+ */
+
+public class TelaRadioMain {
+    public static void main(String[] args) {
+        TelaRadio tr = new TelaRadio();
+
+        tr.showTela();
+    }
+}
